a57b10e551747950d1d8dd10e48be08aa66f6c29
pires12455 <viturinokalil@gmail.com>
2021-03-28 16:31:00 -0400

    First commit

---
+++ Fxc7/help.js
@@ -0,0 +1,365 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal) => { 
+	return `
+	
+
+\`\`\`INSTAGRAM OFFICIAL\`\`\`
+https://www.instagram.com/lordescreamocanal/
+
+
+\`\`\`POR FAVOR, LEIA AS NOTAS ABAIXO ANTES\`\`\`
+\`\`\`USANDO O BOT\`\`\`
+
+╭──────「 *REGULAMENTO ${name}* 」
+┴
+┣⊱  \`\`\`NOMA USUARIO:\`\`\` *${pushname2}*
+┣⊱  \`\`\`VERIFICACAO:\`\`\` ✅
+┣⊱  \`\`\`LIMITE:\`\`\` *${limitt}*
+┣⊱  \`\`\`ATIVO:\`\`\` ${kyun(uptime)}
+┣⊱  \`\`\`HORA:\`\`\` *${jam} WIB*
+┣⊱  \`\`\`ENCONTRO:\`\`\` *${tanggal}*
+┣⊱  \`\`\`VERSAO:\`\`\` *6.5.0*
+┣⊱  \`\`\`USUARIO REGISTRADO:\`\`\` *${user.length} User*
+┣⊱  ❌ *SPAM*
+┣⊱  ❌ *CALL & VC*
+┣⊱  \`\`\`Quebra??\`\`\` *Banido*
+┬
+╰────────────────────────
+
+
+╭──────「 *SOBRE ${name}* 」
+┴
+│➻ *${prefix}report lapor bug* 
+│➻ *${prefix}info*
+│➻ *${prefix}donasi* mendingacao
+│➻ *${prefix}owner*
+│➻ *${prefix}speed*
+│➻ *${prefix}daftar*
+│➻ *${prefix}totaluser*
+│➻ *${prefix}grouplist*
+│➻ *${prefix}blocklist*
+│➻ *${prefix}banlist*
+│➻ *${prefix}premiumlist*
+│➻ *${prefix}bahasa*
+┬
+╰────────────────────────
+
+
+͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭──────「 *MEDIA DOWNLOAD* 」
+┴
+│➻ *${prefix}tiktokstalk username*
+│➻ *${prefix}igstalk 
+│➻ *${prefix}insta Link*
+│➻ *${prefix}instastory username*
+│➻ *${prefix}ssweb url*
+│➻ *${prefix}url2img Url*
+│➻ *${prefix}tiktok*
+│➻ *${prefix}fototiktok*
+│➻ *${prefix}meme*
+│➻ *${prefix}memeindo*
+│➻ *${prefix}kbbi*
+│➻ *${prefix}wait*
+│➻ *${prefix}trendtwit*
+│➻ *${prefix}google berita terkini*
+┬
+╰────────────────────────
+
+
+╭──────「 *CRIADOR MENU* 」
+┴
+│➻ *${prefix}quotemaker tx/wtrmk/tema*
+│➻ *${prefix}nulis nama/kelas/text*
+│➻ *${prefix}rain reply image*
+│➻ *${prefix}trigger reply image*
+│➻ *${prefix}rip reply image*
+│➻ *${prefix}wasted reply image*
+│➻ *${prefix}cphlogo *
+│➻ *${prefix}cglitch *
+│➻ *${prefix}cpubg*
+│➻ *${prefix}cml *
+│
+│➻ *${prefix}tahta *
+│➻ *${prefix}croman *
+│➻ *${prefix}cthunder *
+│➻ *${prefix}cbpink *
+│➻ *${prefix}cmwolf *
+│➻ *${prefix}csky *
+│➻ *${prefix}cwooden *
+│➻ *${prefix}cflower *
+│➻ *${prefix}clove *
+│➻ *${prefix}ccrossfire *
+│➻ *${prefix}cnaruto *
+│➻ *${prefix}cparty *
+│➻ *${prefix}cshadow *
+│➻ *${prefix}cminion *
+│➻ *${prefix}cneon *
+│➻ *${prefix}cneon2 *
+│➻ *${prefix}cneongreen *
+│➻ *${prefix}c3d *
+│➻ *${prefix}csky *
+│➻ *${prefix}tts id Haii*
+│➻ *${prefix}ttp *
+│➻ *${prefix}cballon *
+│➻ *${prefix}cpaper *
+│➻ *${prefix}slide *
+│
+│➻ *${prefix}stiker*
+│➻ *${prefix}gifstiker*
+│➻ *${prefix}toimg*
+│➻ *${prefix}img2url*
+│➻ *${prefix}nobg*
+│➻ *${prefix}tomp3*
+│➻ *${prefix}ocr*
+┬
+╰──────────────────────────
+
+
+╭───────「 *GRUPO APENAS* 」
+┴
+│➻ *${prefix}modeanime On/Off*
+│➻ *${prefix}naruto*
+│➻ *${prefix}minato*
+│➻ *${prefix}boruto*
+│➻ *${prefix}hinata*
+│➻ *${prefix}sakura*
+│➻ *${prefix}sasuke*
+│➻ *${prefix}kaneki*
+│➻ *${prefix}toukachan*
+│➻ *${prefix}rize*
+│➻ *${prefix}akira*
+│➻ *${prefix}itori*
+│➻ *${prefix}kurumi*
+│➻ *${prefix}miku*
+│➻ *${prefix}anime*
+│➻ *${prefix}animecry*
+│➻ *${prefix}neonime*
+│➻ *${prefix}animekiss*
+│➻ *${prefix}wink*
+┬
+╰───────────────────────
+
+╭────────────────────────
+┴
+│➻ *${prefix}welcome On/Off*
+│➻ *${prefix}grup buka/tutup*
+│➻ *${prefix}antilink on/off*
+│➻ *${prefix}ownergrup*
+│➻ *${prefix}setpp*
+│➻ *${prefix}infogc*
+│➻ *${prefix}add*
+│➻ *${prefix}kick*
+│➻ *${prefix}promote*
+│➻ *${prefix}demote*
+│➻ *${prefix}setname* muda nome grupo 
+│➻ *${prefix}setdesc* altera descricao
+│➻ *${prefix}linkgrup*
+│➻ *${prefix}tagme*
+│➻ *${prefix}hidetag* marcar invisivel
+│➻ *${prefix}tagall*
+│➻ *${prefix}mentionall* menciona membro
+│➻ *${prefix}fitnah*
+│➻ *${prefix}listadmin*
+│➻ *${prefix}openanime*
+│➻ *${prefix}edotense*
+┬
+╰────────────────────────
+
+╭────────────────────────
+┴
+│➻ *${prefix}nsfw On/Off*
+│➻ *${prefix}nsfwloli*
+│➻ *${prefix}nsfwblowjob*
+│➻ *${prefix}nsfwneko*
+│➻ *${prefix}nsfwtrap*
+│➻ *${prefix}hentai*
+│➻ *${prefix}simih On/Off*
+┬
+╰────────────────────────
+
+
+╭──────「 *OUTROS DIVERSÃO E JOGO* 」
+┴
+│➻ *${prefix}anjing*
+│➻ *${prefix}kucing*
+│➻ *${prefix}testime*
+│➻ *${prefix}hilih*
+│➻ *${prefix}say*
+│➻ *${prefix}apakah*
+│➻ *${prefix}kapankah*
+│➻ *${prefix}bisakah*
+│➻ *${prefix}rate*
+│➻ *${prefix}watak*
+│➻ *${prefix}hobby*
+│➻ *${prefix}infogempa*
+│➻ *${prefix}infonomor*
+│➻ *${prefix}quotes*
+│➻ *${prefix}truth*
+│➻ *${prefix}dare*
+│➻ *${prefix}katabijak*
+│➻ *${prefix}fakta*
+│➻ *${prefix}darkjokes*
+│➻ *${prefix}bucin*
+│➻ *${prefix}pantun*
+│➻ *${prefix}katacinta*
+│➻ *${prefix}jadwaltvnow*
+│➻ *${prefix}hekerbucin*
+│➻ *${prefix}katailham*
+│➻ *${prefix}animewp*
+┬
+╰────────────────────────
+
+╭──────────────────────────
+┴
+│➻ *${prefix}jarak *
+│➻ *${prefix}translate en / como vai você?*
+│➻ *${prefix}pasangan *
+│➻ *${prefix}gantengcek *
+│➻ *${prefix}cantikcek *
+│➻ *${prefix}artinama *
+│➻ *${prefix}persengay *
+│➻ *${prefix}pbucin *
+│➻ *${prefix}bpfont *
+│➻ *${prefix}textstyle *
+│➻ *${prefix}jadwaltv 
+│➻ *${prefix}lirik 
+│➻ *${prefix}chord 
+│➻ *${prefix}wiki 
+│➻ *${prefix}brainly 
+│➻ *${prefix}resepmasakan 
+│➻ *${prefix}map 
+│➻ *${prefix}film
+│➻ *${prefix}pinterest 
+│➻ *${prefix}infocuaca 
+│➻ *${prefix}jamdunia 
+│➻ *${prefix}mimpi
+│➻ *${prefix}infoalamat 
+│➻ *${prefix}playstore
+┬
+╰───────────────────────────
+
+
+╭────────────────────────
+┴
+│➻ *${prefix}asupan*
+│➻ *${prefix}tebakgambar*
+│➻ *${prefix}caklontong*
+│➻ *${prefix}family100*
+│➻ *${prefix}kalkulator 13*12*
+│➻ *${prefix}wp*
+│➻ *${prefix}moddroid *
+│➻ *${prefix}happymod *
+┬
+╰────────────────────────
+
+╭────────────────────────
+┴
+│➻ *${prefix}cerpen*
+│➻ *${prefix}cersex*
+│➻ *${prefix}xxx japan*
+│➻ *${prefix}pornhub stepMoms* pornozao kkk
+┬
+╰────────────────────────
+
+╭────────────────────────
+┴
+│➻ *${prefix}jadwalsholat Banyuwangi*
+│➻ *${prefix}quran*
+│➻ *${prefix}quransurah 1*
+│➻ *${prefix}tafsir kafir*
+┬
+╰────────────────────────
+
+╭────────────────────────
+┴.   desnecessários
+
+│➻ *${prefix}string becrypt *
+│➻ * $ {prefix} string encode64 *
+│➻ * $ {prefix} decode64 encrypt *
+│➻ *${prefix}encode32 fragmento*
+│➻ * $ {prefix} decode32 encrypt *
+│➻ * $ {prefix} string encbinária *
+│➻ * $ {prefix} criptografia decbinária *
+│➻ * $ {prefix} string encoctal *
+│➻ *${prefix}decoctal criptografar*
+│➻ *${prefix}hashidentifier Criptografar Hash *
+│➻ *${prefix}dorking dork*
+│➻ *${prefix}pastebin teks*
+│➻ *${prefix}tinyurl link*
+│➻ *${prefix}bitly link*
+┬
+╰────────────────────────
+
+╭────────────────────────
+┴
+│➻ *${prefix}spamcall 083xxxxxxxxx*
+│➻ *${prefix}spamsms 083xxxxxxxx/jumlah*
+│➻ *${prefix}spamgmail *
+┬
+╰────────────────────────
+
+
+╭─────────「 *SO PROPRIETARIO* 」
+┴
+│➻ *${prefix}addprem mentioned*
+│➻ *${prefix}removeprem mention*
+│➻ *${prefix}setmemlimit*
+│➻ *${prefix}setreply*
+│➻ *${prefix}setprefix*
+│➻ *${prefix}setnamebot* muda nome desse bot
+│➻ *${prefix}setppbot*
+│➻ *${prefix}bc*
+│➻ *${prefix}bcgc*
+│➻ *${prefix}ban*
+│➻ *${prefix}unban*
+│➻ *${prefix}block*
+│➻ *${prefix}unblock*
+│➻ *${prefix}clearall*
+│➻ *${prefix}delete*
+│➻ *${prefix}clone*
+│➻ *${prefix}getses*
+│➻ *${prefix}leave*
+┬
+╰────────────────────────
+
+
+╭────────「 *PREMIUM APENAS* 」
+┴
+│➻ *${prefix}playmp3 menepi*
+│➻ *${prefix}fb link video*
+│➻ *${prefix}snack link snack video*
+│➻ *${prefix}ytmp3 link yt* ta baixando musicas youTube
+│➻ *${prefix}ytmp4 link yt*
+│➻ *${prefix}joox The Ultimate Monologue*
+│➻ *${prefix}smule Link Video Smule*
+┬
+╰────────────────────────
+
+
+╭─────「 *informações${name}* 」
+│
+║
+╠🐉 *Pires BOT* 
+╠🐉 𝐃𝐎𝐍𝐎: PIRES
+╠🐉 *wa.me/+556996103915*
+╠🐉 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+╰────────────────────────
+║ _📷 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:_ @pireskkkk
+║
+║
+║ 
+║
+║ _🪀𝐆𝐫𝐮𝐩𝐨 𝐝𝐨 𝐰𝐩𝐩:_
+║ _*🌐https://chat.whatsapp.com/CWDE10hFK31JwBN79IWJLB🌐*_
+║`
+}
+
+exports.help = help
+
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
